3a19fd05cd9514a108ceca91e16e728111370411
DavidPeterK <143167841+DavidPeterK@users.noreply.github.com>
2023-09-15 03:26:53 +0200

    first update AddTask

---
+++ script/adTask.js
@@ -0,0 +1,1 @@
+//.prioBtnAktiv onclick-button//
---
+++ script/pikadayDate.js
@@ -0,0 +1,33 @@
+//only for date-input by addTask.html/ Due date//
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dateInput = document.getElementById('datepicker');
+
+    var picker = new Pikaday({
+        field: dateInput,
+        position: 'top left',
+        format: 'DD/MM/YYYY',
+        minDate: new Date(), // Das stellt sicher, dass kein Datum vor dem heutigen Datum ausgewählt werden kann.
+        onSelect: function (date) {
+            const formattedDate = [
+                date.getDate().toString().padStart(2, '0'),
+                (date.getMonth() + 1).toString().padStart(2, '0'),
+                date.getFullYear()
+            ].join('/');
+            dateInput.value = formattedDate; // Verwenden Sie "dateInput" anstelle von "this._field"
+        }
+    });
+
+    dateInput.addEventListener('focus', function () {
+        if (!this.value) {
+            const today = new Date();
+            const formattedDate = [
+                today.getDate().toString().padStart(2, '0'),
+                (today.getMonth() + 1).toString().padStart(2, '0'),
+                today.getFullYear()
+            ].join('/');
+            this.value = formattedDate;
+            picker.show();
+        }
+    });
+});
