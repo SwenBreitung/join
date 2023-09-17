8b9bca847837fca297409cb7499ee30f7a09ca42
SwenBreitung <Veroxa@web.de>
2023-09-17 12:53:09 +0200

    new summery, new help text, summery css new

--- script/summary.js
+++ script/summary.js
@@ -1,41 +1,106 @@
-const users = {
+let userss = {
     "user0": {
         "name": "hello",
         "email": "1234",
-        "password": "12345",
+        "password": "1234",
+        "color": null,
         "cards": {
-            "ToDo0": {
-                "category": "Technical Task",
-                "title": "sad",
-                "text": "sadsad",
-                "time": "sadd",
-                "date": "23.13.23",
-                "priority": "medium",
+            "toDo": {
+                "ToDo0": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "ToDo1": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "ToDo2": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "ToDo3": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "Urgent",
+                }
             },
-            "InProgress0": {
-                "category": "Technical Task",
-                "title": "sad",
-                "text": "sadsad",
-                "time": "sadd",
-                "date": "23.13.23",
-                "priority": "medium",
+            "inProgress": {
+                "inProgress0": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "inProgress1": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "inProgress2": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
             },
-            "AwaitFeedback0": {
-                "category": "Technical Task",
-                "title": "sad",
-                "text": "sadsad",
-                "time": "sadd",
-                "date": "23.13.23",
-                "priority": "medium",
+            "awaitFeedback": {
+                "awaitFeedback0": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "awaitFeedback1": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
             },
-            "Done0": {
-                "category": "Technical Task",
-                "title": "sad",
-                "text": "sadsad",
-                "time": "sadd",
-                "date": "23.13.23",
-                "priority": "Urgent",
-            }
+            "done": {
+                "done0": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+                "done1": {
+                    "category": "Technical Task",
+                    "title": "sad",
+                    "text": "sadsad",
+                    "time": "sadd",
+                    "date": "23.13.23",
+                    "priority": "medium",
+                },
+            },
         },
         "contacts": {
             "contact0": [{
@@ -50,40 +115,104 @@ const users = {
 function init() {
     loadTimeOfDay();
     loadSvgPen();
+    loadText();
 }
 
+
+function loadText() {
+    loadUserName();
+    loadNumersToDo();
+    loadNumersInProgress();
+    loadNumersAwaitFeedback();
+    loadNumersDone();
+    loadNumbersBoard();
+}
+
+//----------------------search function------------------------------
 //---Search User name----------------------------
 function loadUserName() {
     userName = document.getElementById('name')
-    userName.innerText = loadUserName();
+    userName.innerText = userss['user0']['name'];
 }
-
-function loadUserName() {
-    let userName = document.getElementById('name');
-    userName.innerText = searchUserName();
-}
-
-function searchUserName() {}
-
 //================Search User name END==============================
+
+//----------------------search function cards------------------------------
+function loadNumbersBoard() {
+    let toDos = searchNumbers(userss['user0']['cards']['toDo']);
+    let inProgress = searchNumbers(userss['user0']['cards']['inProgress']);
+    let awaitFeedback = searchNumbers(userss['user0']['cards']['awaitFeedback']);
+    let done = searchNumbers(userss['user0']['cards']['done']);
+    let board = document.getElementById('board');
+    board.innerText = (toDos + inProgress + awaitFeedback + done);
+
+}
+
+
+function loadNumersToDo() {
+    let toDos = document.getElementById('to-dos');
+    toDos.innerText = searchNumbers(userss['user0']['cards']['toDo']);
+}
+
+
+function loadNumersInProgress() {
+    let inProgress = document.getElementById('in-progress');
+    inProgress.innerText = searchNumbers(userss['user0']['cards']['inProgress']);
+}
+
+
+function loadNumersAwaitFeedback() {
+    let awaitFeedback = document.getElementById('await-feedback');
+    awaitFeedback.innerText = searchNumbers(userss['user0']['cards']['awaitFeedback']);
+}
+
+
+function loadNumersDone() {
+    let done = document.getElementById('done');
+    done.innerText = searchNumbers(userss['user0']['cards']['done']);
+}
+
+
+function searchNumbers(collection) {
+    let collectionAsJson = collection;
+    let currentNumber = 0;
+    if (Array.isArray(collectionAsJson)) {
+        for (let i = 0; i < collectionAsJson.length; i++) {
+            currentNumber++;
+        }
+    } else {
+        for (let i = 0; i < Object.keys(collectionAsJson).length; i++) {
+            currentNumber++;
+        }
+    }
+    return currentNumber;
+}
+
+
+function searchFirstUrgantDate() {
+
+}
+//======================== search function cards END==============================
+
+
 
 //----------------- load Time of Day------------------
 function loadTimeOfDay() {
     let HoursOfTheDay = document.getElementById('time-of-day');
-    HoursOfTheDay.innerText = getTimeOfDay();
+    HoursOfTheDay.innerHTML = getTimeOfDay();
 }
 
 function getTimeOfDay() {
     const currentHour = new Date().getHours();
 
     if (currentHour >= 0 && currentHour < 6) {
-        return "Schöne Nacht";
+        return '<span class="time-of-day">Schöne </span><span class="time-of-day">Nacht</span>';
     } else if (currentHour >= 6 && currentHour < 12) {
-        return "Guten Morgen";
+        return '<span class="time-of-day">Guten </span> <span class="time-of-day"> Morgen</span>';
+
     } else if (currentHour >= 12 && currentHour < 18) {
-        return "Guten Nachmittag";
+        return '<span class="time-of-day">Guten</span> <span class="time-of-day">Nachmittag</span>';
     } else {
-        return "Guten Abend";
+        return '<span class="time-of-day">Guten  </span class="time-of-day"><span>Abend</span>';
     }
 }
 
@@ -93,8 +222,4 @@ function loadSvgPen() {
     const penSVGContainer = document.getElementById('penContainer'); // Ändern Sie den Namen des Elements, auf das Sie zugreifen möchten
     const penSVGContent = loadPenSvg();
     penSVGContainer.innerHTML = penSVGContent;
-}
-
-function searchFirstUrgantDate() {
-
 }
