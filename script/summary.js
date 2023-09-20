982afe6d1c942c5bd818207c6b26750ac8b182f0
SwenBreitung <Veroxa@web.de>
2023-09-20 15:09:42 +0200

    new search funktion

--- script/summary.js
+++ script/summary.js
@@ -191,7 +191,7 @@ function searchNumbers(collection) {
 
 
 function searchFirstUrgantDate() {
-
+    extractData(userss['user0']['cards'], ['toDo']);
 }
 //======================== search function cards END==============================
 
@@ -220,6 +220,60 @@ function getTimeOfDay() {
 
 //===================load Time of Day END============================
 
+function extractData(obj, category1) {
+    let urgentCount = 0;
+    let latestDate = '00.00.00';
+    let latestDateItem = null;
+
+    for (let category in obj) {
+        for (let task in obj[category1]) {
+            // Zähle die "urgent" Prioritäten
+            if (obj[category][task].priority === "urgent") {
+                urgentCount++;
+            }
+
+            // Vergleiche das Datum
+            let currentDate = obj[category][task].date;
+            if (compareDates(currentDate, latestDate) > 0) {
+                latestDate = currentDate;
+                latestDateItem = obj[category][task];
+            }
+        }
+    }
+
+    return {
+        urgentCount: urgentCount,
+        latestDateItem: latestDateItem
+    };
+}
+
+// Diese Funktion vergleicht zwei Daten im Format "TT.MM.JJ"
+function compareDates(date1, date2) {
+    const [day1, month1, year1] = date1.split('.').map(Number);
+    const [day2, month2, year2] = date2.split('.').map(Number);
+
+    if (year1 !== year2) return year1 - year2;
+    if (month1 !== month2) return month1 - month2;
+    return day1 - day2;
+}
+
+// Beispiel-Objekt
+const tasks = {
+    // ... (Ihr Objekt)
+};
+
+
+function compareDates(date1, date2) {
+    const [day1, month1, year1] = date1.split('.').map(Number);
+    const [day2, month2, year2] = date2.split('.').map(Number);
+
+
+    if (year1 !== year2) return year1 - year2;
+    if (month1 !== month2) return month1 - month2;
+    return day1 - day2;
+}
+
+//------------------------------------------------------
 
 function loadSvgChop() {
     const penSVGContainer = document.getElementById('chop-container'); // Ändern Sie den Namen des Elements, auf das Sie zugreifen möchten
