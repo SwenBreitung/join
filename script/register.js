9e80dd1a75ba473c98137aacaac5a1c2f5c51d45
DavidPeterK <143167841+DavidPeterK@users.noreply.github.com>
2023-10-03 11:41:06 +0200

    save

--- script/register.js
+++ script/register.js
@@ -19,6 +19,22 @@ async function loadUsers() {
     }
 }
 
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch (e) {
+        console.error('Loading error:', e);
+    }
+}
+
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch (e) {
+        console.error('Loading error:', e);
+    }
+}
+
 
 async function register() {
     if (checkbox.checked) {
@@ -30,6 +46,22 @@ async function register() {
             password: password.value,
         });
         await setItem('users', JSON.stringify(users));
+        resetForm();
+        window.location = 'index.html';
+    }
+}
+
+async function registUser() {
+    if (checkbox.checked) {
+        registerBtn.disabled = true;
+
+        user.push({
+            name: userName.value,
+            email: email.value,
+            password: password.value,
+            color: '',
+        });
+        await setItem('userGroup698', JSON.stringify(user));
         resetForm();
         window.location = 'index.html';
     }
